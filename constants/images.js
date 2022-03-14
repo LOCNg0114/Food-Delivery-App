1f7c651f1cc379d1ce4ef348a4ec7e309343b6d5
Tien Loc Nguyen <58358673+LOCNg0114@users.noreply.github.com>
2022-03-15 06:33:16 +0700

    Add files via upload

---
+++ constants/images.js
@@ -0,0 +1,67 @@
+const avatar_1 = require("../assets/images/avatar-1.jpg");
+const avatar_2 = require("../assets/images/avatar-2.jpg");
+const avatar_3 = require("../assets/images/avatar-3.jpg");
+const avatar_4 = require("../assets/images/avatar-4.jpg");
+const avatar_5 = require("../assets/images/avatar-5.jpg");
+const baked_fries = require("../assets/images/baked-fries.jpg");
+const burger_restaurant_1 = require("../assets/images/burger-restaurant.jpg");
+const burger_restaurant_2 = require("../assets/images/burger-restaurant-2.jpg");
+const chicago_hot_dog = require("../assets/images/chicago-hot-dog.jpg");
+const crispy_chicken_burger = require("../assets/images/crispy-chicken-burger.jpg");
+const fries_restaurant = require("../assets/images/fries-restaurant.jpg");
+const hawaiian_pizza = require("../assets/images/hawaiian-pizza.jpg");
+const honey_mustard_chicken_burger = require("../assets/images/honey-mustard-chicken-burger.jpg");
+const hot_dog_restaurant = require("../assets/images/hot-dog-restaurant.jpg");
+const meat_sticks = require("../assets/images/meat-sticks.jpg");
+const japanese_restaurant = require("../assets/images/japanese-restaurant.jpg");
+const deli_house = require("../assets/images/deli-house.jpg");
+const flan_cake = require("../assets/images/flan-cake.jpg");
+const hoanh_thanh = require("../assets/images/hoanh-thanh.jpg");
+const mi_hoanh_thanh = require("../assets/images/mi-hoanh-thanh.jpg");
+const hu_tiu_mi = require("../assets/images/hu-tiu-mi.jpg");
+const acon_house = require("../assets/images/acon-house.jpg");
+const pizza_restaurant = require("../assets/images/pizza-restaurant.jpg");
+const pizza = require("../assets/images/pizza.jpg");
+const salad = require("../assets/images/salad.jpg");
+const mi_vit_tiem = require("../assets/images/mi-vit-tiem.jpg");
+const sushi = require("../assets/images/sushi.jpg");
+const ice_cookies = require("../assets/images/ice-cookies.jpg");
+const tomato_pasta = require("../assets/images/tomato-pasta.jpg");
+const broken_rice = require("../assets/images/broken-rice.jpg");
+const chicken_rice = require("../assets/images/chicken-rice.jpg");
+
+export default {
+  avatar_1,
+  avatar_2,
+  avatar_3,
+  avatar_4,
+  avatar_5,
+  baked_fries,
+  burger_restaurant_1,
+  burger_restaurant_2,
+  chicago_hot_dog,
+  crispy_chicken_burger,
+
+  fries_restaurant,
+  hawaiian_pizza,
+  honey_mustard_chicken_burger,
+  hot_dog_restaurant,
+  meat_sticks,
+
+  japanese_restaurant,
+  deli_house,
+  flan_cake,
+  hoanh_thanh,
+  mi_hoanh_thanh,
+  hu_tiu_mi,
+  acon_house,
+  pizza_restaurant,
+  pizza,
+  mi_vit_tiem,
+  sushi,
+  ice_cookies,
+  tomato_pasta,
+  broken_rice,
+  chicken_rice,
+  salad,
+};
---
+++ constants/maps.js
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY = "AIzaSyAjjJndjXehMs_32ilg0HTpyF3UReyeBmY" ; //"YOUR API KEY"
+
+export default GOOGLE_API_KEY;
